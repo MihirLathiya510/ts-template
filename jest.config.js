de1ad0e55c4596ae1fc3dcdfbcb5037e56cfbd0b
Mihir Lathiya <mihirlathiya@Mihirs-MacBook.local>
2022-08-25 23:54:24 +0530

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/tests/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['node_modules', 'src/database', 'src/test', 'src/types'],
+  reporters: ['default'],
+  globals: { 'ts-jest': { diagnostics: false } },
+  // setupFilesAfterEnv: [''],
+  transform: {}
+}
